445f605eec43b00125a90762d7ceb868b992c4a4
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-07-05 22:51:23 +0100

    Update 3
    Updated loc
    Fixed music shuffle option error

--- lua/tweak_data/levelstweakdata.lua
+++ lua/tweak_data/levelstweakdata.lua
@@ -1,12 +1,7 @@
-if restoration.Options:GetValue("OTHER/TimeOfDay") then
-	RestorationCoreHooks:Post(LevelsTweakData, "init", function(self)
-		self.ukrainian_job.env_params = {color_grading = "color_nice"}
-	end)
-end
 local get_music_event_original_res = LevelsTweakData.get_music_event
 function LevelsTweakData:get_music_event(stage)
     local result = get_music_event_original_res(self, stage)
-    if result and stage == "control" and restoration.Options:GetValue("Extra/MusicShuffle") then
+    if result and stage == "control" and restoration.Options:GetValue("HUD/Extra/MusicShuffle") then
         if self.can_change_music then
             managers.music:check_music_switch()
         else
