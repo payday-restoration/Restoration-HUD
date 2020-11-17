105ea1221e20aaae9a398507f9829810970ab9e5
steam-test1 <34393637+steam-test1@users.noreply.github.com>
2020-11-17 03:07:10 +0000

    Hotfix 2
    See wiki for changelog

--- lua/loc/coreloc.lua
+++ lua/loc/coreloc.lua
@@ -3,6 +3,8 @@ local english = Idstring("english"):key() == SystemInfo:language():key()
 
 if english then
     dofile(ModPath .. "lua/loc/loc.lua")
--- elseif russian then
---     dofile(ModPath .. "lua/sc/loc/locru.lua")
+elseif russian then
+    dofile(ModPath .. "lua/sc/loc/locru.lua")
+else
+    dofile(ModPath .. "lua/loc/loc.lua")
 end
